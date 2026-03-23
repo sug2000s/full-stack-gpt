2edc1dd78d61a942e03a01038d9ae41ec223bf9d
78081 <sug2000s@lgcns.com>
2026-03-24 01:53:33 +0900

    5.6 Uploading Documents

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,4 +1,14 @@
-import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from langchain_community.document_loaders import TextLoader
+from langchain_classic.embeddings import CacheBackedEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_classic.storage import LocalFileStore
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
 import streamlit as st
 
 st.set_page_config(
@@ -6,33 +16,50 @@ st.set_page_config(
     page_icon="📃",
 )
 
+
+@st.cache_data(show_spinner="Embedding file...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    # [Legacy] 기존 방식: UnstructuredFileLoader
+    # 최신 방식: TextLoader 사용
+    loader = TextLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings(
+        base_url=os.getenv("OPENAI_EMBEDDING_BASE_URL"),
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model=os.getenv("OPENAI_EMBEDDING_MODEL"),
+    )
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+Welcome!
 
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+file = st.file_uploader(
+    "Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
 
-
-for message in st.session_state["messages"]:
-    send_message(
-        message["message"],
-        message["role"],
-        save=False,
-    )
-
-
-message = st.chat_input("Send a message to the ai ")
-
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
-
-    with st.sidebar:
-        st.write(st.session_state)
+if file:
+    retriever = embed_file(file)
+    s = retriever.invoke("winston")
+    s
